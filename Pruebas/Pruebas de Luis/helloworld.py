a6ced304f064bc4b67f6171cf4a9beb9e1c4c668
Luis B. <luis_ness@hotmail.com>
2014-11-26 11:58:57 +0100

    Añadidos comentarios al programa.

--- Pruebas/Pruebas de Luis/helloworld.py
+++ Pruebas/Pruebas de Luis/helloworld.py
@@ -1,11 +1,21 @@
+# Importamos el marco de trabajo de aplicaciones web.
 import webapp2
 
+# Definimos un controlador de solicitudes (MainPage).
+#   RequestHandler se encarga de procesar las peticiones y contruir respuestas.
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('Hello, World!')
 
-
+# WSGIApplication se encarga de instanciar las ruas de las solicitudes
+#   entrantes a los manipuladores basados en la URL.
+# Asignamos el controlador de solicitudes (MainPage) a la URL raiz (/), de modo
+#   que cuando 'webapp2' recibe una solicitud 'GET HTTP' a la URL '/' se crea
+#   una instancia de la clase 'MainPage' y llama a al método 'get'.
+# La informacion acerca de la solicitud se puede obtener usando 'self.request'.
+# debug=True sirve para imprimir la traza de la pila en la salida del 
+#   navegador.
 application = webapp2.WSGIApplication([
     ('/', MainPage),
 ], debug=True)
--- cloudsarao-project/main.py
+++ cloudsarao-project/main.py
@@ -1,8 +1,10 @@
 from google.appengine.api import users
 
+# Importamos el marco de trabajo de aplicaciones web.
 import webapp2
 
-
+# Definimos un controlador de solicitudes (MainPage).
+#   RequestHandler se encarga de procesar las peticiones y contruir respuestas.
 class MainPage(webapp2.RequestHandler):
 
     def get(self):
@@ -25,7 +27,15 @@ class Saraos(webapp2.RequestHandler):
     def get(self):
         self.response.write('Web Sarao')
 
-
+# WSGIApplication se encarga de instanciar las ruas de las solicitudes
+#   entrantes a los manipuladores basados en la URL.
+# Asignamos el controlador de solicitudes (MainPage) a la URL raiz (/), de modo
+#   que cuando 'webapp2' recibe una solicitud 'GET HTTP' a la URL '/' se crea
+#   una instancia de la clase 'MainPage' y llama a al método 'get'.
+# La informacion acerca de la solicitud se puede obtener usando 'self.request'.
+# Asignamos el controlador de solicitudes (Saraos) a la URL '/saraos'.
+# debug=True sirve para imprimir la traza de la pila en la salida del 
+#   navegador.
 application = webapp2.WSGIApplication([
     ('/', MainPage),
     ('/saraos', Saraos),
