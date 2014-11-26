cee54bb773b7b7b7840944529b80b5dc76be07ef
Luis B. <luis_ness@hotmail.com>
2014-11-26 12:01:21 +0100

    División de clases.

--- cloudsarao-project/main.py
+++ cloudsarao-project/main.py
@@ -3,8 +3,12 @@ from google.appengine.api import users
 # Importamos el marco de trabajo de aplicaciones web.
 import webapp2
 
-# Definimos un controlador de solicitudes (MainPage).
-#   RequestHandler se encarga de procesar las peticiones y contruir respuestas.
+#==============================================================================
+#==============================================================================
+# # Controlador de solicitudes 'MainPage'.
+#==============================================================================
+#==============================================================================
+# RequestHandler se encarga de procesar las peticiones y contruir respuestas.
 class MainPage(webapp2.RequestHandler):
 
     def get(self):
@@ -23,10 +27,21 @@ class MainPage(webapp2.RequestHandler):
             self.redirect(users.create_login_url(self.request.uri))
         # [END if_not_user]
 
+#==============================================================================
+#==============================================================================
+# # Controlador de solicitudes 'Saraos'.
+#==============================================================================
+#==============================================================================
 class Saraos(webapp2.RequestHandler):
     def get(self):
         self.response.write('Web Sarao')
 
+
+#==============================================================================
+#==============================================================================
+# # Programa principal.
+#==============================================================================
+#==============================================================================
 # WSGIApplication se encarga de instanciar las ruas de las solicitudes
 #   entrantes a los manipuladores basados en la URL.
 # Asignamos el controlador de solicitudes (MainPage) a la URL raiz (/), de modo
