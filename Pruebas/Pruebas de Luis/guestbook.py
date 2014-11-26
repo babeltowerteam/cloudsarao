150b49511fd22319b9dfc7a81d72deb6fdb729ae
Luis B. <luis_ness@hotmail.com>
2014-11-26 12:36:31 +0100

    Cambiada extensión del README de pruebas.
    Además se ha añadido la aplicación de libro de visitas.

--- Pruebas/Pruebas de Luis/guestbook.py
+++ Pruebas/Pruebas de Luis/guestbook.py
@@ -1,3 +1,5 @@
+import cgi
+
 # Importamos el servicio de usuarios para que la aplicacion se integre con las
 #   cuentas de usuario de Google.
 from google.appengine.api import users
@@ -5,25 +7,30 @@ from google.appengine.api import users
 # Importamos el marco de trabajo de aplicaciones web.
 import webapp2
 
+
+MAIN_PAGE_HTML = """\
+<html>
+  <body>
+    <form action="/sign" method="post">
+      <div><textarea name="content" rows="3" cols="60"></textarea></div>
+      <div><input type="submit" value="Sign Guestbook"></div>
+    </form>
+  </body>
+</html>
+"""
+
 # Definimos un controlador de solicitudes (MainPage).
 #   RequestHandler se encarga de procesar las peticiones y contruir respuestas.
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        # Comprobamos que hay una cuenta de Google activa.
-        usuario = users.get_current_user()
-        
-        # Si esta activo.
-        if usuario:
-            self.response.headers['Content-Type'] = 'text/plain'
-            # Le saludamos.
-            self.response.write('Hello,' + usuario.nickname() + ' !')
+        self.response.write(MAIN_PAGE_HTML)
+
             
-        # Si no hay una cuenta activa.
-        else:
-            # Le mandamos a la pagina de login.
-            self.redirect(users.create_login_url(self.request.uri))
-            
-            
+class Guestbook(webapp2.RequestHandler):
+    def post(self):
+        self.response.write('<html><body>You wrote:<pre>')
+        self.response.write(cgi.escape(self.request.get('content')))
+        self.response.write('</pre></body></html>')            
 
 # WSGIApplication se encarga de instanciar las ruas de las solicitudes
 #   entrantes a los manipuladores basados en la URL.
@@ -35,4 +42,5 @@ class MainPage(webapp2.RequestHandler):
 #   navegador.
 application = webapp2.WSGIApplication([
     ('/', MainPage),
+    ('/sign', Guestbook),
 ], debug=True)
