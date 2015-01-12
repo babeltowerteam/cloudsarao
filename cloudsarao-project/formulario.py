9b96a38406c5b15f24a0c346203b9410d29753aa
Álvaro Muñoz <alvaromm@correo.ugr.es>
2015-01-12 16:43:53 +0100

    Clases persistence y formulario

---
+++ cloudsarao-project/formulario.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+CABECERA = """<html><head></head><body>"""
+FIN_CABECERA = """</body></html>"""
+
+class FormularioNuevoSarao():
+  def parseFormulario(self):
+    return CABECERA + """
+        <form action="" method="post">
+            Ingrese su nombre:
+            <input type="text" name="Nombre"><br>
+            <input type="submit" value="Enviar"><br>
+        </form>
+        """ + FIN_CABECERA
+
+
+class FormularioNuevoAsistente():
+    def parseFormulario(self):
+        return ""
---
+++ cloudsarao-project/persistence.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from google.appengine.ext import db
+
+#==============================================================================
+#==============================================================================
+# # Clase Lugar
+#==============================================================================
+#==============================================================================
+class Lugar(db.Model):
+    calle = db.StringProperty(required=True)
+    numero = db.IntegerProperty(required=True)
+    cod_postal = db.IntegerProperty()
+
+
+#==============================================================================
+#==============================================================================
+# # Clase Sarao
+#==============================================================================
+#==============================================================================
+# Esta clase tiene los siguientes atributos:
+# - nombre : Nombre del Sarao.
+# - fecha : Fecha de realizacion del Sarao.
+# - max_asistentes : El numero máximo de asistentes que puede asistir al
+#                   Sarao.
+# - num_asistentes : Número de asistentes que están apuntados al Sarao.
+# - url : Dirección web del Sarao.
+# - nota : Todo tipo de notas aclaratorias asociadas al Sarao.
+# - descripcion : Descripción del evento.
+class Sarao(db.Model):
+  # Atributos del sarao.
+    nombre         = db.StringProperty(required=True)
+    fecha          = db.DateProperty(required=True)
+    hora           = db.DateProperty()
+    max_asistentes = db.IntegerProperty(required=True)
+    num_asistentes = db.IntegerProperty()
+    url            = db.StringProperty(required=True)
+    nota           = db.StringProperty()
+    descripcion    = db.TextProperty()
+    organizacion   = db.StringProperty()
+    # Relación uno-a-muchos
+    lugar          = db.ReferenceProperty(Lugar, collection_name='lugares')
+
+
+#==============================================================================
+#==============================================================================
+# # Clase Asistente
+#==============================================================================
+#==============================================================================
+class Asistente(db.Model):
+    correo = db.EmailProperty(required=True)
+    nombre = db.StringProperty(required=True)
+    nick_twitter = db.StringProperty()
+    colectivo = db.StringProperty(required=True, choices=('alumno', 'PDI', 'PAS', 'otro'))
+    procedencia = db.StringProperty()
+    #Relacion muchos-muchos de saraos y asistentes
+    asistencia_saraos = db.ListProperty(db.Key)
