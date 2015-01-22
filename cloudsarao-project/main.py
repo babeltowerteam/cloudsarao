96abfe7898a5e9e081674a8892931c439ba79260
Daniel Alvarez <danymatic24@gmail.com>
2015-01-22 14:45:41 +0100

    Nueva prueba de redirección.

--- cloudsarao-project/main.py
+++ cloudsarao-project/main.py
@@ -55,13 +55,14 @@ class MainPage(Handler):
 class Administracion(Handler):
   def get(self):
       user = users.get_current_user()
-      if not users.is_current_user_admin():
+      if users.is_current_user_admin():
+          s = Sarao.getSaraos()
+          self.render('pagina_administracion.html', saraos=s)
+          
+      else:
           self.redirect(users.create_logout_url('/'))
           self.redirect(users.create_login_url(self.request.uri))
 
-      else:
-          s = Sarao.getSaraos()
-          self.render('pagina_administracion.html', saraos=s)
 
 
 
