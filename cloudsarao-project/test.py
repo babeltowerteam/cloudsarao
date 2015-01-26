c6dea8fa5d62428a356d5313f57ea3a843fe14aa
Daniel Alvarez <danymatic24@gmail.com>
2015-01-26 17:26:47 +0100

    Test de insertar y elimar sarao y lugar añadidos.

--- cloudsarao-project/test.py
+++ cloudsarao-project/test.py
@@ -1,16 +1,34 @@
 import unittest
 from google.appengine.api import users
-from main import Sarao
+from google.appengine.ext import testbed
+from main import persistence
 
 class SaraoTestCase(unittest.TestCase):
 	def setUp(self):
-		pass
+		self.testbed = testbed.Testbed()
+		self.testbed.activate()
+		self.testbed.init_datastore_v3_stub()
 
-	def test(self):
-		sarao = Sarao()
-		res = sarao.realizaAlgunaOperacionGuay(4)
-		self.assertEqual(res,8)
+		lugar = Lugar(nombre="Lugar de prueba", calle="Calle de prueba", cod_postal=6666)
+		lugar.put()
 
+
+
+	def tearDown(self):
+		self.testbed.desactivate()
+
+
+	def testInsertarSarao(self):
+		sarao = Sarao(nombre="Sarao de prueba", fecha="11\11\2009", max_asistentes=11, lugar = lugar)
+		sarao.put()
+		sarao = Sarao.gql("WHERE nombre = :n", n=sarao.nombre)
+		sarao.delete()
+
+	def testInsertarLugar(self):
+		lugar = Lugar(nombre="testLugar", calle="testCalle", cod_postal=1111)
+		lugar.put()
+		lugar = Lugar.gql("WHERE nombre = :n", n=lugar.nombre)
+		lugar.delete()
 
 if __name__ == "__main__":
 	unittest.main()
